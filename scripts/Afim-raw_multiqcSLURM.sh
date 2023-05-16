58c4863099c9b3ed6b8834242ff7fb8db00e1a6b
letimm <letimm3@gmail.com>
2023-05-16 10:30:22 -0800

    Delete scripts directory
    new reference genome necessitates a total re-run

--- scripts/Afim-raw_multiqcSLURM.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --cpus-per-task=1
-#SBATCH --job-name=multiQC
-#SBATCH --output=/home/ltimm/Anoplopoma_fimbria/job_outfiles/Afim-raw_multiQC.out
-
-source /home/ltimm/bin/hydraQC/bin/activate
-multiqc /home/ltimm/Anoplopoma_fimbria/fastqc/raw/
